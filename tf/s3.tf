f11db58807b6c58aadae6530eff576ab5f205dd4
Marco Antonio Gonzalez Junior <m@rco.sh>
2024-11-17 16:50:18 -0300

    Testing Terraform

--- tf/s3.tf
+++ tf/s3.tf
@@ -1,13 +1,12 @@
 resource "aws_s3_bucket" "this" {
   bucket = var.s3_name
-  block_public_acls = false
-  block_public_policy = false
+  acl = "public-read"
 }
 
-resource "aws_s3_bucket_acl" "this" {
-  bucket = aws_s3_bucket.this.id
-  acl    = "private"
-}
+# resource "aws_s3_bucket_acl" "this" {
+#   bucket = aws_s3_bucket.this.id
+#   acl    = "private"
+# }
 
 resource "aws_s3_bucket_website_configuration" "this" {
   bucket = aws_s3_bucket.this.bucket
